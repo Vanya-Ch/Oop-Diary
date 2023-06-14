fb2072f52357910ecb802f0a35ce3c74f8c6dde1
Vanya-Ch <mr.vanya882@gmail.com>
2023-06-14 21:46:11 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace ООП_курсова
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.newButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButtoon = new System.Windows.Forms.Button();
+            this.diaryView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.diaryView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(214, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(594, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Diary";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.title.Location = new System.Drawing.Point(12, 92);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(100, 23);
+            this.title.TabIndex = 1;
+            this.title.Text = "Title:";
+            // 
+            // description
+            // 
+            this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.description.Location = new System.Drawing.Point(12, 142);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(121, 23);
+            this.description.TabIndex = 2;
+            this.description.Text = "Description:";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(16, 119);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(1054, 20);
+            this.titleTextBox.TabIndex = 3;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(16, 168);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(1054, 20);
+            this.descriptionTextBox.TabIndex = 4;
+            // 
+            // newButton
+            // 
+            this.newButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.newButton.Location = new System.Drawing.Point(16, 195);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(259, 33);
+            this.newButton.TabIndex = 5;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.editButton.Location = new System.Drawing.Point(281, 195);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(259, 33);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.Location = new System.Drawing.Point(546, 195);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(259, 33);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButtoon
+            // 
+            this.saveButtoon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButtoon.Location = new System.Drawing.Point(811, 195);
+            this.saveButtoon.Name = "saveButtoon";
+            this.saveButtoon.Size = new System.Drawing.Size(259, 33);
+            this.saveButtoon.TabIndex = 8;
+            this.saveButtoon.Text = "Save";
+            this.saveButtoon.UseVisualStyleBackColor = true;
+            this.saveButtoon.Click += new System.EventHandler(this.saveButtoon_Click);
+            // 
+            // diaryView
+            // 
+            this.diaryView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.diaryView.BackgroundColor = System.Drawing.Color.White;
+            this.diaryView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.diaryView.Location = new System.Drawing.Point(16, 235);
+            this.diaryView.Name = "diaryView";
+            this.diaryView.Size = new System.Drawing.Size(1054, 351);
+            this.diaryView.TabIndex = 9;
+            // 
+            // Diary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(1076, 587);
+            this.Controls.Add(this.diaryView);
+            this.Controls.Add(this.saveButtoon);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label1);
+            this.Name = "Diary";
+            this.Text = "Diary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.diaryView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButtoon;
+        private System.Windows.Forms.DataGridView diaryView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ООП_курсова
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable diary = new DataTable();
+        bool isEditing = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create columns
+            diary.Columns.Add("Title");
+            diary.Columns.Add("Description");
+
+            // point our datagridview to our datasource
+            diaryView.DataSource = diary;
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Text = "";
+            descriptionTextBox.Text = "";
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            isEditing = true;
+            // fill text fields with data from table
+            titleTextBox.Text = diary.Rows[diaryView.CurrentCell.RowIndex].ItemArray[0].ToString();
+            descriptionTextBox.Text = diary.Rows[diaryView.CurrentCell.RowIndex].ItemArray[1].ToString();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                diary.Rows[diaryView.CurrentCell.RowIndex].Delete();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error :" + ex);
+            }
+        }
+
+        private void saveButtoon_Click(object sender, EventArgs e)
+        {
+            if (isEditing)
+            {
+                diary.Rows[diaryView.CurrentCell.RowIndex]["Title"] = titleTextBox.Text;
+                diary.Rows[diaryView.CurrentCell.RowIndex]["Description"] = descriptionTextBox.Text;
+            }
+            else
+            {
+                diary.Rows.Add(titleTextBox.Text, descriptionTextBox.Text);
+            }
+            // Clear fields
+            titleTextBox.Text = "";
+            descriptionTextBox.Text = "";
+            isEditing = false;
+        }
+    }
+}
